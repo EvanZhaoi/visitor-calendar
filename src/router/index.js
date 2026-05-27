bd875dfe8a5d7e8558ed0f715206e5b55f2a0bc4
EvanZhao <evanzhao@EvanZhaodeMac-mini.local>
2026-05-27 10:04:11 +0800

    refactor: extract App.vue to router-view, rename calendar to CalendarView

--- src/router/index.js
+++ src/router/index.js
@@ -1,9 +1,9 @@
 import { createRouter, createWebHashHistory } from 'vue-router'
-import App from '../App.vue'
+import CalendarView from '../CalendarView.vue'
 import GanttView from '../GanttView.vue'
 
 const routes = [
-  { path: '/', component: App },
+  { path: '/', component: CalendarView },
   { path: '/gantt', component: GanttView },
 ]
 
